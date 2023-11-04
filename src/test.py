dc11f853a066315b74b6afa11043edfc50e5c853
Lucius <lucius@czlucius.dev>
2023-11-05 00:34:45 +0800

    Restructure values

--- src/test.py
+++ src/test.py
@@ -6,14 +6,19 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 segments = [ 63, 6, 91, 79, 102, 109, 125, 7, 127, 111 ]
 
 @cocotb.test()
-async def test_7seg(dut):
+async def test_alu(dut):
     dut._log.info("start")
                     #   yyyyxxxx
                     #   01230123
-    dut.ui_in.value = 0b10111001
+    dut.clk.value = 0
+    dut.ena.value = 1
+    dut.uio_out.value = 0x0
+    dut.uo_out.value = 0x0
 
     dut._log.info("reset")
     dut.rst_n.value = 1
+
+    dut.ui_in.value = 0b10111001
 
     dut.uio_in.value = 0
     assert dut.uo_out.value == 22
