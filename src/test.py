74d15a48538df48ab80071c2996e2b5baf88124d
Lucius <lucius@czlucius.dev>
2023-11-04 23:43:02 +0800

    Use .value in tests

--- src/test.py
+++ src/test.py
@@ -13,53 +13,53 @@ async def test_7seg(dut):
     dut.ui_in.value = 0b10111001
 
     dut.uio_in.value = 0
-    assert dut.uo_out == 22
+    assert dut.uo_out.value == 22
 
     # subtract may be a little finicky
     dut.uio_in.value = 1
     dut.log_info("subtract out")
     dut.log_info(dut.uo_out.value)
-    assert dut.uo_out == 0b11111100
+    assert dut.uo_out.value == 0b11111100
 
     dut.uio_in.value = 2
-    assert dut.uo_out == 117
+    assert dut.uo_out.value == 117
 
     dut.uio_in.value = 3
     dut.ui_in.value = 0b10011011
     dut.log_info("divide out")
     dut.log_info(dut.uo_out.value)
-    assert dut.uo_out == 1
+    assert dut.uo_out.value == 1
 
     dut.uio_in.value = 4
     dut.ui_in.value = 0b10011011
-    assert dut.uo_out == 0b1001
+    assert dut.uo_out.value == 0b1001
 
     dut.uio_in.value = 5
-    assert dut.uo_out == 0b1101
+    assert dut.uo_out.value == 0b1101
 
 
     dut.uio_in.value = 6
-    assert dut.uo_out == 0b0100
+    assert dut.uo_out.value == 0b0100
 
     dut.uio_in.value = 7
-    assert dut.uo_out == 0b0110
+    assert dut.uo_out.value == 0b0110
 
     dut.uio_in.value = 8
-    assert dut.uo_out == 0b0010
+    assert dut.uo_out.value == 0b0010
 
     dut.uio_in.value = 9
-    assert dut.uo_out == 0b01000110
+    assert dut.uo_out.value == 0b01000110
 
     dut.uio_in.value = 10
-    assert dut.uo_out == 4
+    assert dut.uo_out.value == 4
 
     dut.ui_in.value = 11
     dut.ui_in.value = 0b1100_0110
-    assert dut.uo_out == 48
+    assert dut.uo_out.value == 48
 
     dut.ui_in.value = 12
     dut.ui_in.value = 0b1000_0110
-    assert dut.uo_out == 3
+    assert dut.uo_out.value == 3
 
 
 
