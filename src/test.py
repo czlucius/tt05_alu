6a1a7443e29175431ff17385f18db7ab70ad552a
Lucius <lucius@czlucius.dev>
2023-11-05 01:48:51 +0800

    experiment

--- src/test.py
+++ src/test.py
@@ -81,7 +81,7 @@ async def test_alu(dut):
     # 0000
 
     #00000101
-    dut.ui_in.value = 0b01100101 
+    dut.ui_in.value = 0b01100111 
 
     await Timer(20, units="ns")
     assert dut.uo_out.value == 48 
