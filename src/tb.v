84b2e77c227ac546f9ed907c59304344423bb2bc
Lucius <lucius@czlucius.dev>
2023-11-04 23:18:45 +0800

    Correct module name (tb.v)

--- src/tb.v
+++ src/tb.v
@@ -28,7 +28,7 @@ module tb ();
     wire [7:0] uio_out;
     wire [7:0] uio_oe;
 
-    tt_um_seven_segment_seconds tt_um_seven_segment_seconds (
+    tt_um_czlucius_alu tt_um_czlucius_alu (
     // include power ports for the Gate Level test
     `ifdef GL_TEST
         .VPWR( 1'b1),
